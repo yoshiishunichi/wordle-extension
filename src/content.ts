ae58d880da55485a03add95ac90e81e0d77a83f7
吉井駿一 <syun_syun01@icloud.com>
2022-02-21 11:47:56 +0900

    fix url

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-const gameState = localStorage.getItem("gameState");
+const gameState = localStorage.getItem("nyt-wordle-state");
 if (gameState) {
   const gameStateData = JSON.parse(gameState) as { solution: string };
   window.alert(`こたえ: ${gameStateData.solution.toUpperCase()}`);
